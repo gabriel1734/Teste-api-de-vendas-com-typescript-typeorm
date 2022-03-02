66bd180fa6cb8a62e2841b2f02a352e2c6d4b898
Gabriel <gabrielfurniel.gc@outlook.com>
2022-03-02 15:59:36 -0300

    show profile

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  user_id: string;
+}
+class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.', 404);
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
